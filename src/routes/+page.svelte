62c237ce0a386a6b4ce6d742a4ce32f7db1c43a8
Carlos Nascimento <carlosnascimento.ms@outlook.com>
2025-06-21 16:12:00 -0300

    Inicial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+  <script>
+  function funcao() {
+      alert("Aqui na quebrada os função nós entrega pros 190, parça");
+  }
+
+
+
+
+</script>
+    
+
+
+<h1>
+  Iniciar
+  <img src="/if.png" alt="Logo do Instituto Federal Pernambuco" />
+</h1>
+
+<p>
+  Bem-vindo | Welcome | Bienvenue | Willkommen | Bienvenido | Benvenuto | ようこそ | 欢迎 | Добро пожаловать |
+  أهلاً وسهلاً | 환영합니다 | स्वागत है
+</p>
+
+<hr />
+<br />
+
+<button on:click={() => navegar('jogo.html')}>Iniciar Jogo</button>
+<br /><br />
+<button on:click={() => navegar('sobre.html')}>Sobre o Jogo</button>
+<br /><br />
+<button on:click={() => navegar('ajuda.html')}>Ajuda</button>
+<br /><br />
+<button on:click={() => navegar('ranking.html')}>Ranking</button>
+<br /><br />
+<button on:click={() => navegar('sair.html')}>Sair</button>
+<br /><br />
+<button on:click={funcao}>Executar Função</button>
+
+<hr />
+
